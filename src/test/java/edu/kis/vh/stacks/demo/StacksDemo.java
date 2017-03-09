039999fb5bbb633f9f08b55b5c84f73725cc2915
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-03-09 15:05:03 +0100

    poprawa bledow formatowania kodu

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -12,23 +12,33 @@ class StacksDemo {
 		stack[] stacks = { factory.GetStandardStack(), factory.GetFalseStack(),
 				factory.GetFIFOStack(), factory.GetHanoiStack()};
 		
-		for (int i = 1; i < 15; i++)
-			for (int j = 0; j < 3; j++)
+		for (int i = 1; i < 15; i++) {
+			for (int j = 0; j < 3; j++) {
 				stacks[j].push(i);
-			
-			java.util.Random rn = new java.util.Random();
-			for (int i = 1; i < 15; i++)
-				stacks[3].push(rn.nextInt(20));
-			
-			for (int i = 0; i < stacks.length; i++) {
-				while (!stacks[i].isEmpty())
-					System.out.print(stacks[i].pop() + "  ");
-					System.out.println();
 			}
+		}
+
+		java.util.Random rn = new java.util.Random();
+		for (int i = 1; i < 15; i++) {
+			stacks[3].push(rn.nextInt(20));
+		}
+
+		for (int i = 0; i < stacks.length; i++) {
+			while (!stacks[i].isEmpty()) {
+				System.out.print(stacks[i].pop() + "  ");
+			}
+			System.out.println();
+		}
 		
-		System.out.println("total rejected is "
-				+ ((StackHanoi) stacks[3]).reportRejected());
-		
+		System.out.println("total rejected is " + ((StackHanoi) stacks[3]).reportRejected());
 	}
-	
 }
+/*
+Linia 15: dodanie klamer otwierających i zamykających dla zwiekszenia czytelnosci co dokladnie
+           nalezy do ktorej petli for
+Linia 19-27: przesuniecie tabulacji o jeden w lewo
+Linia 22: dodanie klamer dla petli for
+Linia 27: dodanie klamer dla petli while
+Linia 33: przeniesienie calego wypisania do jednej linii
+*/
+
