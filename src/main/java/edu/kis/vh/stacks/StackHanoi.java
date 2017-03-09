897e6082a5ae4c72154d8a8a880faf681791a963
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-03-09 15:23:52 +0100

    refactor klasy StackHanoi

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -2,16 +2,18 @@ package edu.kis.vh.stacks;
 
 public class StackHanoi extends Stack {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 	return totalRejected;
 	}
 
 	public void push(int in) {
-	if (!isEmpty() && in > top())
-		totalRejected++;
-		else
+		if (!isEmpty() && in > top()) {
+			totalRejected++;
+		}
+		else {
 			super.push(in);
+		}
 	}
 }
