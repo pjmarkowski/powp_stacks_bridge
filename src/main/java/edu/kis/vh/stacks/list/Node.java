ae2ca6fc7bb363e587a5e3fa3554bd8712353df0
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-03-11 18:19:30 +0100

    hermetyzacja w klasie Node

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,11 +2,39 @@ package edu.kis.vh.stacks.list;
 
 class Node {
 
-	int value;
-	Node prev, next;
+	private int value;
+	private Node prev;
+	private Node next;
+
+//Po hermetyzacji tych pól w klasie StackList dostęp do tych pól automatycznie zmienił się na dostęp za pomocą getterow
+
+
 
 	Node(int i) {
-		value = i;
+		setValue(i);
 	}
-	
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 }
