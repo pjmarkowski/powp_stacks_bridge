4d77e4c497e3fe56cd47a0b510073c71ea9d785f
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-03-15 23:59:49 +0100

    Test dla metody  pushElement dla klasy StackList

---
+++ src/test/java/edu/kis/vh/stacks/unittests/stackListTest.java
@@ -0,0 +1,24 @@
+package edu.kis.vh.stacks.unittests;
+
+import edu.kis.vh.stacks.list.StackList;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Piotr on 15.03.2017.
+ */
+public class stackListTest {
+    private StackList stackList = new StackList();
+
+    @Test
+    public void shouldPushElement() {
+        final int VALUE = 3;
+
+        stackList.pushElement(VALUE);
+
+        assertThat(VALUE, is(stackList.peek()));
+    }
+}
