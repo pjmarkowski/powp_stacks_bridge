2734e0288501547b010793bbc54a44dc20e7774a
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-03-29 19:42:24 +0200

    optymalizacja StackFIFO + komentarz

--- src/main/java/edu/kis/vh/stacks/StackFIFO.java
+++ src/main/java/edu/kis/vh/stacks/StackFIFO.java
@@ -1,5 +1,6 @@
 package edu.kis.vh.stacks;
 
+import edu.kis.vh.stacks.implementations.StackArray;
 import edu.kis.vh.stacks.implementations.StackList;
 
 public class StackFIFO extends Stack {
@@ -11,7 +12,11 @@ public class StackFIFO extends Stack {
 	public StackFIFO() {
 	}
 
-	private final Stack temp = new Stack();
+	private final Stack temp = new Stack(new StackList());
+
+    /**
+     *Najlepszym wyborem bedzie uzycie StackList
+     */
 
 	@Override
 	public int pop() {
